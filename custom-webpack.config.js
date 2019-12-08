2c8b87bdaa682034a247bae3b8b2ac642e356840
wsyski <wsyski@gmail.com>
2019-12-08 22:36:34 +0100

    Initial version

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,7 +1,11 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   plugins: [
+    new CopyWebpackPlugin([
+      path.join(__dirname, 'angular-dll', 'angular-dll.js')
+    ]),
     new webpack.DllReferencePlugin({
       context: __dirname,
       manifest: require(path.join(__dirname, 'angular-dll', 'angular-manifest.json'))
