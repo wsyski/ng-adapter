f34d3eafe2aec4e6c78c2c3fffc246749d675612
wsyski <wsyski@gmail.com>
2019-12-14 17:21:57 +0100

    Initial version

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,34 +1,34 @@
-var webpack = require("webpack")
-var path = require("path")
+var webpack = require("webpack");
+var path = require("path");
 
 module.exports = {
-    mode: "production",
-    // context: __dirname,
-    // resolve: {
-    //     extensions: [".js", ".jsx"]
-    // },
-    entry: {
-        angular: [
-            "@angular/animations",
-            "@angular/core",
-            "@angular/common",
-            "@angular/forms",
-            "@angular/router",
-            "@angular/platform-browser",
-            "@angular/platform-browser-dynamic",
-            "rxjs"
-        ],
-        // ui: []
-    },
-    output: {
-        path: path.join(__dirname, "./dll"),
-        filename: "[name]-dll-es5.js",
-        library: "[name]"
-    },
-    plugins: [
-        new webpack.DllPlugin({
-            path: path.join(__dirname, "./dll/", "[name]-manifest.json"),
-            name: "[name]"
-        })
-    ]
+  mode: "development",
+  optimization: {
+    minimize: false,
+    noEmitOnErrors: true
+  },
+  entry: {
+    angular: [
+      "@angular/animations",
+      "@angular/core",
+      "@angular/common",
+      "@angular/forms",
+      "@angular/router",
+      "@angular/platform-browser",
+      "@angular/platform-browser-dynamic",
+      "rxjs"
+    ],
+    // ui: []
+  },
+  output: {
+    path: path.join(__dirname, "./dll"),
+    filename: "[name]-dll-es5.js",
+    library: "[name]"
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      path: path.join(__dirname, "./dll/", "[name]-manifest.json"),
+      name: "[name]"
+    })
+  ]
 };
