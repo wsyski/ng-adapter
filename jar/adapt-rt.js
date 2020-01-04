d230b5375848cd516e3d9f4f0f2f4441bbaf9e94
wsyski <wsyski@gmail.com>
2020-01-04 17:50:46 +0100

    Initial version

--- jar/adapt-rt.js
+++ jar/adapt-rt.js
@@ -1,12 +1,9 @@
-Liferay.Loader.define("ng-adapter@1.0.0/adapt-rt", ['module', 'exports', 'require'], function (module, exports, require) {
-	var define = undefined;
-	var PATH_CONTEXT = Liferay.ThemeDisplay.getPathContext();
+var PATH_CONTEXT = Liferay.ThemeDisplay.getPathContext();
 
-	function adaptStaticURL(url) {
-		return PATH_CONTEXT + "/o/ng-adapter-1.0.0/ng-adapter/" + url;
-	}
+function adaptStaticURL(url) {
+  return PATH_CONTEXT + "/o/ng-adapter-1.0.0/ng-adapter/" + url;
+}
 
-	module.exports = {
-		adaptStaticURL
-	};
-});
+module.exports = {
+  adaptStaticURL
+};
--- jar/index.js
+++ jar/index.js
@@ -1,18 +1,15 @@
-Liferay.Loader.define("ng-adapter@1.0.0/index", ['module', 'exports', 'require', 'portlet-dll', './ng-adapter/polyfills', './ng-adapter/polyfills-es5', './ng-adapter/runtime', './ng-adapter/main', './adapt-rt'], function (module, exports, require) {
-  var define = undefined;
-  const polyfills = require("./ng-adapter/polyfills");
-  const runtime = require("./ng-adapter/runtime");
-  const main = require("./ng-adapter/main");
-  const _ADAPT_RT_ = require("./adapt-rt");
-  const portletDll = Arena.portletDll;
+const polyfills = require("./ng-adapter/polyfills");
+const runtime = require("./ng-adapter/runtime");
+const main = require("./ng-adapter/main");
+const _ADAPT_RT_ = require("./adapt-rt");
+const portletDll = Arena.portletDll;
 
-  if (Liferay.Browser.isIe()) {
-    const polyfillsEs5 = require("./ng-adapter/polyfills-es5");
-    polyfillsEs5(null, null, portletDll);
-  }
-  polyfills(null, null, portletDll);
-  runtime(null, null, portletDll);
-  module.exports = function (_LIFERAY_PARAMS_) {
-    main(_LIFERAY_PARAMS_, _ADAPT_RT_, portletDll);
-  };
-});
+if (Liferay.Browser.isIe()) {
+  const polyfillsEs5 = require("./ng-adapter/polyfills-es5");
+  polyfillsEs5(null, null, portletDll);
+}
+polyfills(null, null, portletDll);
+runtime(null, null, portletDll);
+module.exports = function (_LIFERAY_PARAMS_) {
+  main(_LIFERAY_PARAMS_, _ADAPT_RT_, portletDll);
+};
