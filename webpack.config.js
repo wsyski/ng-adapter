4991c89172c4b0140b18dea62753beebaa349756
wsyski <wsyski@gmail.com>
2020-01-12 11:06:33 +0100

    Initial version

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,19 @@ const pkg = require("./package.json");
 module.exports = (config, options) => {
 
   config.plugins.push(
-    new CopyWebpackPlugin([
-      path.join(__dirname, 'dll', pkg.name, entryName + '-dll.js'),
-      path.join(__dirname, 'dev', 'js', 'liferay.js')
-    ]),
+    new webpack.ContextReplacementPlugin(
+      /\@angular(\\|\/)core(\\|\/)fesm5/,
+      path.join(__dirname, './src')
+    ),
     new webpack.DllReferencePlugin({
       context: __dirname,
       manifest: require(path.join(__dirname, 'dll', pkg.name, entryName + '-dll-manifest.json')),
       name: entryName
     }),
-    new webpack.ContextReplacementPlugin(
-      /\@angular(\\|\/)core(\\|\/)fesm5/,
-      path.join(__dirname, './src')
-    )
+    new CopyWebpackPlugin([
+      path.join(__dirname, 'dll', pkg.name, entryName + '-dll.js'),
+      path.join(__dirname, 'dev', 'js', 'liferay.js')
+    ])
   );
 
   return config;
